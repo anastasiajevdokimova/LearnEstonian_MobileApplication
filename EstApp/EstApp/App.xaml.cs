398529791e2001d304d600f06867afe3efa42484
anastasiajevdokimova <a.jevdokimova98@gmail.com>
2022-05-23 11:10:37 +0300

    #182048888-create-database

--- EstApp/EstApp/App.xaml.cs
+++ EstApp/EstApp/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,6 +7,22 @@ namespace EstApp
 {
     public partial class App : Application
     {
+        public const string DATABASE_NAME = "words.db";
+        public static DBRepository database;
+
+        public static DBRepository Database
+        {
+            get
+            {
+                if(database == null)
+                {
+                    database = new DBRepository(
+                        Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
